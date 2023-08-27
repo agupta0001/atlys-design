c7e2797b6a5bb4bbc5ae995162d85afbd2f8dca5
Ashutosh Gupta <ashutoshgupta312@gmail.com>
2023-08-27 19:52:16 +0530

    Clean Up

---
+++ components/MainLayout.tsx
@@ -0,0 +1,9 @@
+function MainLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <main className="w-[100vw] h-[100vh]">
+            {children}
+        </main>
+    );
+}
+
+export default MainLayout;
